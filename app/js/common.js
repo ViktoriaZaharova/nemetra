5f7afb1d39a3b80e5e1a2e479b7b2184a34bb662
Viktoria <zaharova181114@gmail.com>
2021-11-08 23:30:59 +0200

    Initial commit

---
+++ app/js/common.js
@@ -0,0 +1,39 @@
+// активная ссылка меню
+$('.menu li a').each(function () {
+    let location = window.location.href;
+    let link = this.href;
+    if (location === link) {
+        $(this).addClass('active');
+    }
+});
+// end
+
+// menu
+$('.btn-burger').on('click', function () {
+   $('.mobile-menu').fadeToggle();
+});
+
+$('.btn-close-menu').on('click', function () {
+    $('.mobile-menu').fadeOut();
+});
+
+
+$('.latest-blog-slider').slick({
+    slidesToShow: 3,
+    prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
+    nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
+    responsive: [
+        {
+            breakpoint: 1100,
+            settings: {
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+            }
+        }
+    ]
+});
